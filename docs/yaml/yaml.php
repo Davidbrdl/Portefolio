8a149d14d35e08194f6035292789ec8bf48e15d9
Davidbrdl <david.bourdel@sts-sio-caen.info>
2024-01-04 17:36:14 +0100

    yaml

--- docs/yaml/yaml.php
+++ docs/yaml/yaml.php
@@ -8,11 +8,13 @@ require_once('vendor/autoload.php');
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
 
-function yaml_parse_file($file){
-	try {
-		return Yaml::parseFile($file);
-	} catch (ParseException $exception) {
-		printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
+if (!function_exists('yaml_parse_file')){
+	function yaml_parse_file($file){
+		try {
+			return Yaml::parseFile($file);
+		} catch (ParseException $exception) {
+			printf('Unable to parse the YAML string: <b>%s</b>', $exception->getMessage());
+		}
 	}
 }
 
